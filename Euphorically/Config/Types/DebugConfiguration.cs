f5ca4850db688b67783b4e3bcd15d3fddcf9cb19
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2024-09-14 04:00:31 +0100

    WIP: Reworking Configuration
    Changed how the mod configuration file is handled internally entirely, allowing for saving to and loading from the file at runtime - allowing for a easier time modifying values, without having to restart SHVDN entirely, causing several seconds of freeze.

---
+++ Euphorically/Config/Types/DebugConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using GTA;
+
+namespace Euphorically.Config.Types
+{
+    internal class DebugConfiguration : IConfig
+    {
+        public void Save(ScriptSettings settings)
+        {
+            settings.SetValue(nameof(DebugConfiguration), nameof(ShowDebugNotifications), ShowDebugNotifications);
+            settings.SetValue(nameof(DebugConfiguration), nameof(PedSearchRadius), PedSearchRadius);
+        }
+
+        public void Load(ScriptSettings settings)
+        {
+            ShowDebugNotifications = settings.GetValue(nameof(DebugConfiguration), nameof(ShowDebugNotifications), false);
+            PedSearchRadius = settings.GetValue(nameof(DebugConfiguration), nameof(PedSearchRadius), 10.0f);
+        }
+        
+        public bool ShowDebugNotifications { get; set; }
+        public float PedSearchRadius { get; set; }
+    }
+}
---
+++ Euphorically/Config/Types/ForceConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using GTA;
+
+namespace Euphorically.Config.Types
+{
+    public class ForceConfiguration : IConfig
+    {
+        public void Save(ScriptSettings settings)
+        {
+            settings.SetValue(nameof(ForceConfiguration), nameof(UseAdditiveEuphoriaForce), UseAdditiveEuphoriaForce);
+            settings.SetValue(nameof(ForceConfiguration), nameof(BaseEuphoriaForce), BaseEuphoriaForce);
+            settings.SetValue(nameof(ForceConfiguration), nameof(UseRandomEuphoriaForce), UseRandomEuphoriaForce);
+            settings.SetValue(nameof(ForceConfiguration), nameof(MinimumRandomEuphoriaForce), MinimumRandomEuphoriaForce);
+            settings.SetValue(nameof(ForceConfiguration), nameof(MaximumRandomEuphoriaForce), MaximumRandomEuphoriaForce);
+        }
+
+        public void Load(ScriptSettings settings)
+        {
+            UseAdditiveEuphoriaForce = settings.GetValue(nameof(ForceConfiguration), nameof(UseAdditiveEuphoriaForce), true);
+            BaseEuphoriaForce = settings.GetValue(nameof(ForceConfiguration), nameof(BaseEuphoriaForce), 10.0f);
+            UseRandomEuphoriaForce = settings.GetValue(nameof(ForceConfiguration), nameof(UseRandomEuphoriaForce), true);
+            MinimumRandomEuphoriaForce = settings.GetValue(nameof(ForceConfiguration), nameof(MinimumRandomEuphoriaForce), 5.0f);
+            MaximumRandomEuphoriaForce = settings.GetValue(nameof(ForceConfiguration), nameof(MaximumRandomEuphoriaForce), 15.0f);
+        }
+        
+        public bool UseAdditiveEuphoriaForce { get; set; }
+        //TODO: Determine if this is the way to go with Euphoria Additive Forces - such as predefined forces for different weapon types
+        public float BaseEuphoriaForce { get; set; }
+        public bool UseRandomEuphoriaForce { get; set; }
+        public float MinimumRandomEuphoriaForce { get; set; }
+        public float MaximumRandomEuphoriaForce { get; set; }
+    }
+}
