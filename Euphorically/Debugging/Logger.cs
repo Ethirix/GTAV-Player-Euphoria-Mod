a554e7b1c858f4c3c3634c5f471d815ff6cac9bf
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2024-09-14 04:02:33 +0100

    WIP: Logging to File
    Started looking into a Logging rework, as in the previous version it was simply an in-game 'text' notification, in a function directly in the `Main` class. Currently a prototype File logging is present, however how this functions will be changed - as I want to allow users to enable the Debugging, instead of them being compiled out - which I would do in a normal application.

---
+++ Euphorically/Debugging/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Euphorically.Debugging
+{
+    internal class Logger
+    {
+        [Conditional("DEBUG")]
+        public static void LogToFile(string message)
+        {
+            File.AppendAllText("Euphorically.log", message + Environment.NewLine);
+        }
+    }
+}
